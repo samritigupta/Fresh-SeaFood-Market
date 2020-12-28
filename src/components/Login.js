b003cc645518a7eaa68a874b277fc9e151ad15a3
samritigupta <gupta.samriti1991@gmail.com>
2020-12-28 00:55:08 +0000

    spell correction

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ const Login = (props) => (
         <h2>Inventory Login</h2>
         <p>Sign in to manage your store's inventory</p>
         <button className="github" onClick={() => props.authenticate('Github')}>Login With Github</button>
-        <button className="facebook" onClick={() => props.authenticate('Facebook')}>Login With Facebbok</button>
+        <button className="facebook" onClick={() => props.authenticate('Facebook')}>Login With Facebook</button>
     </nav>
 );
 
